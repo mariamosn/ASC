e1861e4f063ad6ec7b7d1bf3020a5543e9797650
Maria <mariamosneag@gmail.com>
2022-04-27 20:12:26 +0300

    neopt + blas done

--- assignments/tema2/skel/solver_blas.c
+++ assignments/tema2/skel/solver_blas.c
@@ -2,7 +2,11 @@
  * Tema 2 ASC
  * 2022 Spring
  */
+#include <stdio.h>
+#include <string.h>
+
 #include "utils.h"
+#include "cblas.h"
 
 #define DIE(assertion, call_description)				\
 	do {								\
@@ -19,5 +23,73 @@
  */
 double* my_solver(int N, double *A, double *B) {
 	printf("BLAS SOLVER\n");
-	return NULL;
+
+	// calcul B x A
+	// Obs. A = matrice superior triunghiulară
+	double *BxA = calloc(N * N, sizeof(double));
+	DIE(BxA == NULL, "BxA calloc error");
+
+	memcpy(BxA, B, N * N * sizeof(double));
+
+	// inițial BxA = B,
+	// după efectuarea înmulțirii, BxA = B x A
+	cblas_dtrmm(
+		CblasRowMajor,
+		// ordinea în care se înmulțesc A și B (B x A)
+		CblasRight,
+		// A este superior triunghiulară
+        CblasUpper,
+		CblasNoTrans,
+        CblasNonUnit,
+		// număr de linii
+		N,
+		// număr de coloane
+		N,
+        1,
+		A, N,
+        BxA, N
+	);
+
+	// calcul (B x A) x A_T
+	// Obs. A_T = matrice inferior triunghiulară
+	double *BxAxA_T;
+
+	cblas_dtrmm(
+		CblasRowMajor,
+		// ordinea în care se înmulțesc B x A și A_T ((B x A) x A_T)
+		CblasRight,
+		// A este superior triunghiulară
+        CblasUpper,
+		// înmulțirea se face cu A_T
+		CblasTrans,
+        CblasNonUnit,
+		// număr de linii
+		N,
+		// număr de coloane
+		N,
+        1,
+		A, N,
+        BxA, N
+	);
+
+	BxAxA_T = BxA;
+
+	// calcul B_T x B + B x A x A_T
+	cblas_dgemm(
+		CblasRowMajor,
+		// B_T
+		CblasTrans,
+		// B
+        CblasNoTrans,
+		N, N, N,
+		1,
+		B, N,
+		B, N,
+        1,
+		BxAxA_T, N
+	);
+
+	double *C = BxAxA_T;
+
+	return C;
 }
